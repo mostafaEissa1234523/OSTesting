da328a7fdcf13d90e28ea4ef551c32182966e963
Mostafa Eissa <mostfa1234523@gmail.com>
2023-12-27 19:15:26 +0200

    changing some in the update file

--- update/app.js
+++ update/app.js
@@ -2,9 +2,13 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello new update!\n');
+  res.send('Hello from update!\n');
 });
 
+app.get('/update', function (req, res) {
+    res.send('Hello new update1!\n');
+  });
+  
 app.listen(8080, function () {
   console.log('Example app  on port 8080!');
 });
